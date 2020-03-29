f88cc158a86666b4f2c25e329166ae1ca5678faf
dionysis9901 <dionysis.koufis@gmail.com>
2020-03-29 17:18:17 +0300

    Updates

---
+++ precache-manifest.e2044351a07cf0572aa1c1576a386cf7.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7703e5ccf3d85408af8fdb14905b4259",
+    "url": "/Poker-React-Redux-RXJS/index.html"
+  },
+  {
+    "revision": "b9d0b2f0d9ce981d827b",
+    "url": "/Poker-React-Redux-RXJS/static/css/main.672b1c22.chunk.css"
+  },
+  {
+    "revision": "1a4afc0d746cdb44b0f0",
+    "url": "/Poker-React-Redux-RXJS/static/js/2.ca3a619e.chunk.js"
+  },
+  {
+    "revision": "4f012439ffb435e71eef882a66a83922",
+    "url": "/Poker-React-Redux-RXJS/static/js/2.ca3a619e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b9d0b2f0d9ce981d827b",
+    "url": "/Poker-React-Redux-RXJS/static/js/main.2d9fd83f.chunk.js"
+  },
+  {
+    "revision": "6cccc7b9375667359e39",
+    "url": "/Poker-React-Redux-RXJS/static/js/runtime-main.2059786f.js"
+  },
+  {
+    "revision": "cde8fef58015159f6e51b47fd90b62b4",
+    "url": "/Poker-React-Redux-RXJS/static/media/JC.cde8fef5.gif"
+  },
+  {
+    "revision": "d55f47650481b9d38ae41ef1655f30f0",
+    "url": "/Poker-React-Redux-RXJS/static/media/JD.d55f4765.gif"
+  },
+  {
+    "revision": "345d4b58e85429b47fce2730cfb9b5e6",
+    "url": "/Poker-React-Redux-RXJS/static/media/JH.345d4b58.gif"
+  },
+  {
+    "revision": "1c54970fc9d08833b682c3d243d02142",
+    "url": "/Poker-React-Redux-RXJS/static/media/JS.1c54970f.gif"
+  },
+  {
+    "revision": "44311f677c0e9800d42266e80085847a",
+    "url": "/Poker-React-Redux-RXJS/static/media/KC.44311f67.gif"
+  },
+  {
+    "revision": "678e9fb786dd5aaf97ef4e3112953ff5",
+    "url": "/Poker-React-Redux-RXJS/static/media/KD.678e9fb7.gif"
+  },
+  {
+    "revision": "9865961f3a679ff39d46dfa7771d5544",
+    "url": "/Poker-React-Redux-RXJS/static/media/KH.9865961f.gif"
+  },
+  {
+    "revision": "3570dadb6bfb645cb0de24eb729f9244",
+    "url": "/Poker-React-Redux-RXJS/static/media/KS.3570dadb.gif"
+  },
+  {
+    "revision": "b6d6d0ee7fe4bb3a1eb87fd6c43d14d2",
+    "url": "/Poker-React-Redux-RXJS/static/media/QC.b6d6d0ee.gif"
+  },
+  {
+    "revision": "59da7805f0b5f3d63378ad8559bce3b8",
+    "url": "/Poker-React-Redux-RXJS/static/media/QD.59da7805.gif"
+  },
+  {
+    "revision": "931438a28457198c5d902652d56574de",
+    "url": "/Poker-React-Redux-RXJS/static/media/QH.931438a2.gif"
+  },
+  {
+    "revision": "8bcef36a3ccfc13adafc3d354e2bfd14",
+    "url": "/Poker-React-Redux-RXJS/static/media/QS.8bcef36a.gif"
+  },
+  {
+    "revision": "c4b9c7b471809a55af19bdab02df20e6",
+    "url": "/Poker-React-Redux-RXJS/static/media/poker.c4b9c7b4.png"
+  }
+]);
